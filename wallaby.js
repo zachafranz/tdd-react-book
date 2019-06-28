520d2dea57cdf3f0326c4fa7afdf61ece6087b24
Zach Franz <zfranz@bonobos.com>
2019-06-27 22:41:35 -0400

    :wrench: Initial Wallaby setup

---
+++ wallaby.js
@@ -0,0 +1,13 @@
+module.exports = function(wallaby) {
+  return {
+    testFramework: 'jest',
+    env: {
+      type: 'node',
+    },
+    tests: ['src/tests/**/*.test.js'],
+    files: ['src/**/*.js', '!**/*.test.js', '!**/.*'],
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+    },
+  };
+};
